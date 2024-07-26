687381bee0a4c369a7444f9c9dcc0aa0b115b7db
akshay-khapare <139696100+akshay-khapare@users.noreply.github.com>
2024-07-26 20:54:00 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ def predict_next_candle_direction(data):
     
     # Calculate short-term and long-term moving averages
     closing_prices = [candle['close'] for candle in data]
-    short_term_ma = calculate_moving_average(closing_prices, period=1)
-    long_term_ma = calculate_moving_average(closing_prices, period=2)
+    short_term_ma = calculate_moving_average(closing_prices, period=2)
+    long_term_ma = calculate_moving_average(closing_prices, period=4)
     
     # Determine trend direction based on moving averages
     if short_term_ma > long_term_ma:
@@ -38,13 +38,13 @@ def predict_next_candle_direction(data):
     
     # Combine VPT with trend to predict direction
     if vpt < 0 and trend == 'up':
-        return 'call'  # Positive VPT and bullish trend
+        return 'down'  # Positive VPT and bullish trend
     elif vpt > 0 and trend == 'down':
-        return 'put'  # Negative VPT and bearish trend
+        return 'up'  # Negative VPT and bearish trend
     else:
         # Fallback to last candle's direction if no clear signal
         last_candle = data[-1]
-        return 'up' if last_candle['close'] > last_candle['open'] else 'down'if last_candle['close'] < last_candle['open'] else 'neutral'
+        return 'call' if last_candle['close'] > last_candle['open'] else 'put'if last_candle['close'] < last_candle['open'] else 'neutral'
 
 
 app = Flask(__name__)
@@ -59,7 +59,7 @@ API.connect()
 def predict():
     pair = request.args.get('pair', 'EURUSD')
     timeframe =int(request.args.get('timeframe', 1))
-    offset = int(request.args.get('offset', 4))
+    offset = int(request.args.get('offset', 6))
     signal = ''
     trend = ''
     percent = ''
